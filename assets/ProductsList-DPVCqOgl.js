353d276968430ff952b757f339241aac2515a678
Viktoriia <vikapazyuk@gmail.com>
2026-01-08 13:14:50 +0200

    Updates

---
+++ assets/ProductsList-DPVCqOgl.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-B5vJYzaG.js";import{P as o}from"./ProductCard-Cot9dadz.js";const m=({products:t,displayType:i})=>s.jsx("div",{className:"productsList",children:t.map(r=>s.jsx("div",{className:"productItem",children:s.jsx(o,{product:r,displayType:i})},r.id))});export{m as P};
--- assets/index-DGbahUaQ.js
+++
@@ -1,1 +0,0 @@
-import{j as s,L as e}from"./index-B238YTL-.js";const a=()=>s.jsxs("div",{className:"container",children:[s.jsx("p",{className:"text",children:"Page not found"}),s.jsx(e,{className:"link",to:"/",children:"Go to HomePage"})]});export{a as NotFoundPage};
